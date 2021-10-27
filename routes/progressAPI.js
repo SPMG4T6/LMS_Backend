275f607e07dda19c0fc71b1c01350a36d3a41b89
Jason <70564653+BioxMech@users.noreply.github.com>
2021-10-27 19:35:31 +0800

    Fixed the progress endpoint and added a new status returning endpoint

--- routes/progressAPI.js
+++ routes/progressAPI.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Progress = require('../models/progress');
+const Section = require('../models/section');
 
 /**
  * @swagger
@@ -42,14 +43,70 @@ router.get('/progress', function (req, res, next) {
  *      '500':
  *        description: Server error
  */
-router.get('/progress/:userID', function (req, res, next) {
+router.get('/progress/:userID', async function (req, res, next) {
+  let searchResult = await Progress.findOne({ courseCode: req.body.courseCode, className: req.body.className, sectionName: req.body.sectionName });
+
+  if (!searchResult) {
     Progress.find({ userID: req.params.userID })
-        .then(function (progress) {
-            res.status(200).send(progress)
-        })
-        .catch(res => {
-            res.status(500).send({ "message": "Server error" })
-        });
+    .then(function (progress) {
+        res.status(200).send(progress)
+    })
+    .catch(res => {
+        res.status(500).send({ "message": "Server error" })
+    });
+  } else {
+    res.status(400).send({ message: `courseCode: ${req.body.courseCode}, className: ${req.body.className}, sectionName: ${req.body.sectionName}, already exists!` })
+  }
+    
+});
+
+/**
+ * @swagger
+ * /progress/{courseCode}/{className}/{userID}:
+ *  get:
+ *    summary: GET all progress by of a specific user within a class of a course
+ *    tags: [progress]
+ *    parameters:
+ *        - in: path
+ *          name: courseCode
+ *          schema:
+ *            type: string
+ *          required: true
+ *          example: CS555
+ *          description: The course code of the course
+ *        - in: path
+ *          name: className
+ *          schema:
+ *            type: string
+ *          required: true
+ *          example: G1
+ *          description: The class name within the course
+ *        - in: path
+ *          name: userID
+ *          schema:
+ *            type: string
+ *          required: true
+ *          example: 1
+ *          description: A user's ID
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '500':
+ *        description: Server error
+ */
+// Return an array of sections in a course & class of a specific user
+router.get('/progress/:courseCode/:className/:userID', function (req, res, next) {
+  Progress.find({
+    userID: req.params.userID,
+    courseCode: req.params.courseCode,
+    className: req.params.className,
+  })
+  .then(function (progress) {
+    res.status(200).send(progress)
+  })
+  .catch(res => {
+    res.status(500).send({ "message": "Server error" })
+  });
 });
 
 /**
@@ -89,19 +146,91 @@ router.get('/progress/:userID', function (req, res, next) {
  *      '500':
  *        description: Server error
  */
-router.get('/progress/:courseCode/:className/:sectionName/:userID', function (req, res, next) {
-    Progress.find({
+router.get('/progress/:courseCode/:className/:sectionName/:userID', async function (req, res, next) {
+    let doc = await Progress.findOne({
         userID: req.params.userID,
         courseCode: req.params.courseCode,
         className: req.params.className,
         sectionName: req.params.sectionName
     })
-        .then(function (progress) {
-            res.status(200).send(progress)
-        })
-        .catch(res => {
-            res.status(500).send({ "message": "Server error" })
-        });
+
+    if (doc) {
+      res.status(200).send(doc)
+    } else {
+      res.status(404).send({ message: "Progress not found" })
+    }
+});
+
+/**
+ * @swagger
+ * /progress/status/{courseCode}/{className}/{sectionName}/{userID}:
+ *  get:
+ *    summary: Get all progress by specific user within a specific section of a class of a course
+ *    tags: [progress]
+ *    parameters:
+ *        - in: path
+ *          name: courseCode
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The course code of the course
+ *        - in: path
+ *          name: className
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The class name within the course
+ *        - in: path
+ *          name: sectionName
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The section name within the class of the course
+ *        - in: path
+ *          name: userID
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: A user's ID
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '500':
+ *        description: Server error
+ */
+router.get('/progress/status/:courseCode/:className/:sectionName/:userID', async function (req, res, next) {
+  let doc = await Progress.findOne({
+    userID: req.params.userID,
+    courseCode: req.params.courseCode,
+    className: req.params.className,
+    sectionName: req.params.sectionName
+  })
+
+  if (doc) {
+    if (doc.isSectionQuizComplete) {
+      res.status(200).send({ status: true })
+    } else {
+      
+      let isComplete = false;
+      let sectionDoc = await Section.findOne({ 
+        courseCode: req.params.courseCode,
+        className: req.params.className,
+        sectionName: req.params.sectionName
+      })
+
+      if (sectionDoc) {
+        if (sectionDoc.sectionMaterial.length === doc.sectionMaterialName.length && doc.isSectionQuizComplete) {
+          isComplete = true;
+        } 
+
+        res.status(200).send({ status: isComplete })
+      } else {
+        res.status(404).send("Section do not exist");
+      }
+    }
+  } else {
+    res.status(404).send({ message: "Progress not found" })
+  }
 });
 
 /**
@@ -144,13 +273,13 @@ router.get('/progress/:courseCode/:className/:sectionName/:userID', function (re
  */
 // add a new progress to database
 router.post('/progress', function (req, res, next) {
-    Progress.create(req.body)
-        .then(function (progress) {
-            res.status(200).send(progress);
-        })
-        .catch(res => {
-            res.status(500).send({ "message": "Server error" })
-        });
+  Progress.create(req.body)
+    .then(function (progress) {
+        res.status(200).send(progress);
+    })
+    .catch(res => {
+        res.status(500).send({ "message": "Server error" })
+    });
 });
 
 // /**
